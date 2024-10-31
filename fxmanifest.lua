d3b59b37f760b75da50546a3083443b73b015181
MaDHouSe <madhouse1979@gmail.com>
2024-10-31 21:14:30 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ games {'gta5'}
 
 author 'MaDHouSe79'
 description 'MH Vehicle Handeling'
-version '1.0'
+version '1.0.0'
 files {'data/handling.meta'}
 server_scripts {'server/update.lua'}
 data_file 'HANDLING_FILE' 'data/handling.meta'
